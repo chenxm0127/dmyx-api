11fe2e02496412fb902a51f6e68c440542c993ce
chenxm <pro@prodeMacBook-Pro.local>
2023-06-12 22:29:22 +0800

    feat:实现签名功能

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,4 @@
+import * as crypto from 'crypto';
 /**
  * 生成9位伪随机数
  */
@@ -17,3 +18,28 @@ export function generateMessageId(): string {
   const messageId = timestamp + randomNum.toString(); // 结合时间戳和随机数生成消息 ID
   return messageId;
 }
+
+export function signature(
+  header: Record<string, string>,
+  bodyStr: string,
+  secret: string,
+): string {
+  const keyList = Object.keys(header).sort();
+  const kvList = keyList.map((key) => `${key}=${header[key]}`);
+  const urlParams = kvList.join('&');
+  const rawData = urlParams + bodyStr + secret;
+  const md5Result = crypto.createHash('md5').update(rawData).digest();
+  return Buffer.from(md5Result).toString('base64');
+}
+
+const header = {
+  'x-nonce-str': '123456',
+  'x-timestamp': '456789',
+  'x-roomid': '268',
+  'x-msg-type': 'live_gift',
+};
+const bodyStr = 'abc123你好111';
+const secret = 'agorasecret';
+
+const signatureResult = signature(header, bodyStr, secret);
+console.log(signatureResult);
